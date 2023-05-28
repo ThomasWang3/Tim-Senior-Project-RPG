89e717e05dff724a265ab376fce6bea7bcd25158
Thomas Wang <thomaswang.3333@gmail.com>
2023-05-28 09:24:08 -0700

    Playtest Build for Yanman's professor

--- Assets/Scripts/Game Functions/InitiateBattle.cs
+++ Assets/Scripts/Game Functions/InitiateBattle.cs
@@ -5,13 +5,14 @@ using UnityEngine.SceneManagement;
 
 public class InitiateBattle : MonoBehaviour
 {
-    [SerializeField] private BoxCollider2D collider;
+    [SerializeField] private BoxCollider2D enemyCollider;
+    [SerializeField] private List<EntityData> entities;
     //[SerializeField] Rigidbody2D rb;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         Debug.Log("Load battle scene with " + gameObject.name + " vs. " + collision.gameObject.name);
-        //SceneManager.LoadScene("Battle Template", )
+        SceneManager.LoadScene("Battle Template");
     }
 
 }
